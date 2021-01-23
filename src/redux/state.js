62c27b53d9e87fb5ba5cece2c148eb0d21f448a9
Alina Bychkova <ialina240200@gmail.com>
2021-01-23 15:43:41 +0300

    32 - action to props (posts)

--- src/redux/state.js
+++ src/redux/state.js
@@ -37,4 +37,13 @@ let state = {
   }
 }
 
+export let addPost = (postMessage) => {
+  debugger
+  let newPost = {
+    id: 5,
+    message: postMessage
+  }
+  state.profilePage.posts.push(newPost)
+}
+
 export default state
