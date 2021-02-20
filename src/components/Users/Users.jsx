7ef0e767d42a9a7eef2e5e0c15e89f8c882bc026
Alina Bychkova <ialina240200@gmail.com>
2021-02-20 18:28:56 +0300

    51 - add users api

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,55 +1,14 @@
+import * as axios from "axios";
 import s from "./Users.module.css";
+import default_avatar_small from '../../assets/images/default_avatar_small.png'
 
 const Users = (props) => {
   if (props.users.length === 0) {
-    props.setUsers([
-
-      {
-        id: 1,
-        avatarUrl: 'https://i.mycdn.me/i?r=AzEOxUXG5QgodWC3x6hM10Ckx0BZLGOfgD6nXhJoLZbA4MAmq-mVtRg1TeCwydjhl-Q&fn=sqr_288',
-        fullName: 'Alex',
-        status: 'I am a boss',
-        location: {
-          city: 'Moscow',
-          country: 'Russia'
-        },
-        isFollowed: false
-      },
-      {
-        id: 2,
-        avatarUrl: 'https://i.mycdn.me/i?r=AzEOxUXG5QgodWC3x6hM10Ckx0BZLGOfgD6nXhJoLZbA4MAmq-mVtRg1TeCwydjhl-Q&fn=sqr_288',
-        fullName: 'Tony',
-        status: 'I want to eat',
-        location: {
-          city: 'Omsk',
-          country: 'Russia'
-        },
-        isFollowed: false
-      },
-      {
-        id: 3,
-        avatarUrl: 'https://i.mycdn.me/i?r=AzEOxUXG5QgodWC3x6hM10Ckx0BZLGOfgD6nXhJoLZbA4MAmq-mVtRg1TeCwydjhl-Q&fn=sqr_288',
-        fullName: 'Kate',
-        status: 'meow',
-        location: {
-          city: 'Minsk',
-          country: 'Belarus'
-        },
-        isFollowed: true
-      },
-      {
-        id: 4,
-        avatarUrl: 'https://i.mycdn.me/i?r=AzEOxUXG5QgodWC3x6hM10Ckx0BZLGOfgD6nXhJoLZbA4MAmq-mVtRg1TeCwydjhl-Q&fn=sqr_288',
-        fullName: 'Oleg',
-        status: ':3',
-        location: {
-          city: 'Saint-Petersburg',
-          country: 'Russia'
-        },
-        isFollowed: false
-      },
-    ])
+    axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+      props.setUsers(response.data.items)
+    })
   }
+
   return (
     <div className={s.page_wrapper}>
       {
@@ -57,7 +16,7 @@ const Users = (props) => {
           <div key={u.id} className={s.user_wrapper}>
             <div className={s.user_actions}>
               <div className={s.avatar_wrapper}>
-                <img src={u.avatarUrl} className={s.avatar} />
+                <img src={u.photos.small != null ? u.photos.small : default_avatar_small} className={s.avatar} />
               </div>
               <div className={s.btn_wrapper}>
                 <button className={s.btn} onClick={() => { props.toggleFollow(u.id) }}>{u.isFollowed ? 'Unfollow' : 'Follow'}</button>
@@ -65,10 +24,10 @@ const Users = (props) => {
             </div>
             <div className={s.user_bio}>
               <div className={s.bio}>
-                <div className={s.username}>{u.fullName}</div>
+                <div className={s.username}>{u.name}</div>
                 <div className={s.location}>
-                  <div>{u.location.city}</div>
-                  <div>{u.location.country}</div>
+                  <div>{'u.location.city'}</div>
+                  <div>{'u.location.country'}</div>
                 </div>
               </div>
               <div className={s.status}>{u.status}</div>
